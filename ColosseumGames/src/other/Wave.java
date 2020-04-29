c204178258375f0568ee3d91c3f97e6867e6b686
Richard Huang <63317612+CheeezAir@users.noreply.github.com>
2020-04-28 19:53:16 -0700

    Put in some framework for wave

--- ColosseumGames/src/other/Wave.java
+++ ColosseumGames/src/other/Wave.java
@@ -1,5 +1,50 @@
 package other;
+import java.util.ArrayList;
+
+import characters.Enemy;
+import enemies.*;
 
 public class Wave {
+	private int wave;
+	public Wave() {
+		wave = 1;
+		
+	}
+	
+	public void startWave() {
+		ArrayList<Enemy> enemies = new ArrayList<Enemy>();
+		BigBoi bigBoi;
+		MediumBoi mediumBoi;
+		SmallBoi smallBoi;
+		
+		if(wave == 1) {
+			enemies.add(new SmallBoi("small.png", 20, 20, 20, 20, 20, 20, 20, 20, 20));
+			//and so on
+		}
+		else if(wave == 2) {
 
+		}
+		else if(wave == 3) {
+
+		}
+		else if(wave == 4) {
+
+		}
+		else if(wave == 5) {
+
+		}
+		else if(wave == 6) {
+
+		}
+		else if(wave == 7) {
+
+		}
+
+	}
+	public void setWave(int newWave){
+		this.wave = newWave;
+	}
+	public int getWave() {
+		return wave;
+	}
 }
