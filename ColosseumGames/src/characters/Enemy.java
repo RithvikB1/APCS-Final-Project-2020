c8428d6f67423eed987120bd23d7eb8cd6bdf77e
Richard Huang <63317612+CheeezAir@users.noreply.github.com>
2020-04-30 15:48:36 -0700

    behave method for enemy

--- ColosseumGames/src/characters/Enemy.java
+++ ColosseumGames/src/characters/Enemy.java
@@ -1,13 +1,32 @@
 package characters;
 
 import characters.Character;
+import processing.core.PApplet;
 
 public abstract class Enemy extends Character {
-
 	public Enemy(String spriteFile, double speed, double atkSpeed, double HP, double range, double damage, int x, int y,
 			int w, int h) {
 		super(spriteFile, speed, atkSpeed, HP, range, damage, x, y, w, h);
 		// TODO Auto-generated constructor stub
 	}
-	
+	public void behave(Character c, PApplet marker, int enemyType) {
+		int vx = 0;
+		int vy = 0;
+		if(enemyType == 1) {
+			this.shoot((int)c.getX(), (int)c.getY(), marker);
+			if(c.getX() - this.getX() < 0) {
+				vx = -1;
+			}
+			if(c.getX() - this.getX() > 0) {
+				vx = 1;
+			}
+			if(c.getY() - this.getY() > 0) {
+				vy = 1;
+			}
+			if(c.getY() - this.getY() < 0) {
+				vy = -1;
+			}
+			this.walk(vx, vy);
+		}
+	}
 }
