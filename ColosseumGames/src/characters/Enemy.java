a1dde60a08310bb4337bcd1bf3547decdb06b8e9
Richard Huang <63317612+CheeezAir@users.noreply.github.com>
2020-05-03 17:47:16 -0700

    enemy javadocs

--- ColosseumGames/src/characters/Enemy.java
+++ ColosseumGames/src/characters/Enemy.java
@@ -2,13 +2,24 @@ package characters;
 
 import characters.Character;
 import processing.core.PApplet;
-
+/**
+ * 
+ * @author Richard, Rithvik, Aditya
+ *
+ */
 public abstract class Enemy extends Character {
+	
 	public Enemy(String spriteFile, double speed, double atkSpeed, double HP, double range, double damage, int x, int y,
 			int w, int h) {
 		super(spriteFile, speed, atkSpeed, HP, range, damage, x, y, w, h);
 		// TODO Auto-generated constructor stub
 	}
+	/**
+	 * Dictates how an enemy will behave
+	 * @param c character the enemy wants to kill
+	 * @param marker draws the enemy
+	 * @param enemyType the type of enemy
+	 */
 	public void behave(Character c, PApplet marker, int enemyType) {
 		int vx = 0;
 		int vy = 0;
