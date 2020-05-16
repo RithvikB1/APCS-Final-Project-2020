5d5b88d206dfb07aa6a28f28f8776a2f40424235
Aditya Senthilvel <adityasenthilvel@gmail.com>
2020-05-16 14:16:27 -0700

    Update Hercules.java

--- ColosseumGames/src/heroes/Hercules.java
+++ ColosseumGames/src/heroes/Hercules.java
@@ -7,6 +7,8 @@ import processing.core.PApplet;
 import processing.core.PImage;
 
 public class Hercules extends Hero {
+	
+	private ArrayList<PImage> images;
 
 	public Hercules(ArrayList<PImage> images, double speed, double atkSpeed, double HP, double range, double damage,
 			int x, int y, int w, int h) {
@@ -16,31 +18,29 @@ public class Hercules extends Hero {
 
 	@Override
 	public void setup(PApplet marker) {
-		marker.loadImage("sprites/Heroes/Hercules/HerculesFacingRight.png");
-		marker.loadImage("sprites/Heroes/Hercules/HerculesFacingFront.png");
-		marker.loadImage("sprites/Heroes/Hercules/HerculesFacingBack.png");
-		marker.loadImage("sprites/Heroes/Hercules/HerculesFacingLeft.png");			
-		marker.loadImage("sprites/Heroes/Hercules/HerculesAttackBack1.png");
-		marker.loadImage("sprites/Heroes/Hercules/HerculesAttackBack2.png");
-		marker.loadImage("sprites/Heroes/Hercules/HerculesAttackFront1.png");
-		marker.loadImage("sprites/Heroes/Hercules/HerculesAttackFront2.png");
-		marker.loadImage("sprites/Heroes/Hercules/HerculesAttackLeft1.png");
-		marker.loadImage("sprites/Heroes/Hercules/HerculesAttackLeft2.png");
-		marker.loadImage("sprites/Heroes/Hercules/HerculesAttackRight1.png");
-		marker.loadImage("sprites/Heroes/Hercules/HerculesAttackRight2.png");
-		marker.loadImage("sprites/Heroes/Hercules/HerculesWalkBack1.png");
-		marker.loadImage("sprites/Heroes/Hercules/HerculesWalkBack2.png");
-		marker.loadImage("sprites/Heroes/Hercules/HerculesWalkFront1.png");
-		marker.loadImage("sprites/Heroes/Hercules/HerculesWalkFront2.png");
-		marker.loadImage("sprites/Heroes/Hercules/HerculesWalkLeft1.png");
-		marker.loadImage("sprites/Heroes/Hercules/HerculesWalkLeft2.png");
-		marker.loadImage("sprites/Heroes/Hercules/HerculesWalkRight1.png");
-		marker.loadImage("sprites/Heroes/Hercules/HerculesWalkRight2.png");
-		marker.loadImage("sprites/Heroes/Hercules/HerculesSelected.png");
-		marker.loadImage("sprites/Heroes/Hercules/HerculesSelected.png");
-		
-		
-		marker.loadImage("sprites/Heroes/Hercules/PRESelectedHercules.png");
+		images.add(marker.loadImage("sprites/Heroes/Hercules/HerculesFacingRight.png"));
+		images.add(marker.loadImage("sprites/Heroes/Hercules/HerculesFacingFront.png"));
+		images.add(marker.loadImage("sprites/Heroes/Hercules/HerculesFacingBack.png"));
+		images.add(marker.loadImage("sprites/Heroes/Hercules/HerculesFacingLeft.png"));			
+		images.add(marker.loadImage("sprites/Heroes/Hercules/HerculesAttackBack1.png"));
+		images.add(marker.loadImage("sprites/Heroes/Hercules/HerculesAttackBack2.png"));
+		images.add(marker.loadImage("sprites/Heroes/Hercules/HerculesAttackFront1.png"));
+		images.add(marker.loadImage("sprites/Heroes/Hercules/HerculesAttackFront2.png"));
+		images.add(marker.loadImage("sprites/Heroes/Hercules/HerculesAttackLeft1.png"));
+		images.add(marker.loadImage("sprites/Heroes/Hercules/HerculesAttackLeft2.png"));
+		images.add(marker.loadImage("sprites/Heroes/Hercules/HerculesAttackRight1.png"));
+		images.add(marker.loadImage("sprites/Heroes/Hercules/HerculesAttackRight2.png"));
+		images.add(marker.loadImage("sprites/Heroes/Hercules/HerculesWalkBack1.png"));
+		images.add(marker.loadImage("sprites/Heroes/Hercules/HerculesWalkBack2.png"));
+		images.add(marker.loadImage("sprites/Heroes/Hercules/HerculesWalkFront1.png"));
+		images.add(marker.loadImage("sprites/Heroes/Hercules/HerculesWalkFront2.png"));
+		images.add(marker.loadImage("sprites/Heroes/Hercules/HerculesWalkLeft1.png"));
+		images.add(marker.loadImage("sprites/Heroes/Hercules/HerculesWalkLeft2.png"));
+		images.add(marker.loadImage("sprites/Heroes/Hercules/HerculesWalkRight1.png"));
+		images.add(marker.loadImage("sprites/Heroes/Hercules/HerculesWalkRight2.png"));
+		images.add(marker.loadImage("sprites/Heroes/Hercules/HerculesSelected.png"));
+		images.add(marker.loadImage("sprites/Heroes/Hercules/HerculesSelected.png"));
+		images.add(marker.loadImage("sprites/Heroes/Hercules/PRESelectedHercules.png"));
 
 		
 	}
