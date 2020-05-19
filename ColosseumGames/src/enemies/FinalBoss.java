0fe7a55abd732a4a4b057f1a778f5140861d39cc
Richard Huang <63317612+CheeezAir@users.noreply.github.com>
2020-05-19 10:20:02 -0700

    began final boss behavior

--- ColosseumGames/src/enemies/FinalBoss.java
+++ ColosseumGames/src/enemies/FinalBoss.java
@@ -15,6 +15,8 @@ import processing.core.PImage;
  *
  */
 public class FinalBoss extends Enemy {
+	double angle;
+	private ArrayList<PImage> images;
 
 	/**
 	 * Creates an Enemy
@@ -32,20 +34,34 @@ public class FinalBoss extends Enemy {
 			int y, int w, int h) {
 		super(speed, atkSpeed, HP, range, damage, x, y, w, h);
 		// TODO Auto-generated constructor stub
+		angle = 0;
 	}
 
 	@Override
 	public void setup(PApplet marker) {
 		// TODO Auto-generated method stub
-		
+		images = new ArrayList<>();
+		images.add(marker.loadImage("sprites/Enemies/Harpy/HarpyAttackRight1.png"));
+		this.setImages(images);
+
 	}
 
 	@Override
 	public void behave(Character c, PApplet marker) {
 		// TODO Auto-generated method stub
 		
-	}
+		marker.noFill();
+		marker.arc((float)x, (float)y,(float)getRange()*2, (float)getRange()*2, 0, 2*(float)Math.PI);
+		marker.line((float)(getRange()*Math.cos(angle) + x), (float)(getRange()*Math.sin(angle) + y), (float)(getRange()*Math.cos(angle + Math.PI/3) + x), (float)(getRange()*Math.sin(angle + Math.PI/3) + y));
+		marker.line((float)(getRange()*Math.cos(angle + Math.PI/3) + x), (float)(getRange()*Math.sin(angle + Math.PI/3) + y), (float)(getRange()*Math.cos(angle + 2*Math.PI/3) + x), (float)(getRange()*Math.sin(angle + 2*Math.PI/3) + y));
+		marker.line((float)(getRange()*Math.cos(angle + 2*Math.PI/3) + x), (float)(getRange()*Math.sin(angle + 2*Math.PI/3) + y), (float)(getRange()*Math.cos(angle + Math.PI) + x), (float)(getRange()*Math.sin(angle + Math.PI) + y));
+		marker.line((float)(getRange()*Math.cos(angle + Math.PI) + x), (float)(getRange()*Math.sin(angle + Math.PI) + y), (float)(getRange()*Math.cos(angle + 4*Math.PI/3) + x), (float)(getRange()*Math.sin(angle + 4*Math.PI/3) + y));
+		marker.line((float)(getRange()*Math.cos(angle + 4*Math.PI/3) + x), (float)(getRange()*Math.sin(angle + 4*Math.PI/3) + y), (float)(getRange()*Math.cos(angle + 5*Math.PI/3) + x), (float)(getRange()*Math.sin(angle + 5*Math.PI/3) + y));
 
+		angle+= 0.05;
+	}
+	
+	
 	@Override
 	public int getImageNumber() {
 		// TODO Auto-generated method stub
