d2a68af5f1fc6550863e01fe64150c9a139caf87
Richard Huang <63317612+CheeezAir@users.noreply.github.com>
2020-05-25 15:13:58 -0700

    helen nerfed

--- ColosseumGames/src/heroes/Helen.java
+++ ColosseumGames/src/heroes/Helen.java
@@ -15,7 +15,7 @@ public class Helen extends Hero {
 	private boolean position;
 	private int imageNumber;
 	
-	public static final int SPEED = 20, ATK_SPEED = 10, HP = 60000, RANGE = 400, DAMAGE = 500;
+	public static final int SPEED = 20, ATK_SPEED = 10, HP = 60000, RANGE = 400, DAMAGE = 100;
 	public static final int UP_SPEED = 5, UP_ATK_SPEED = 10, UP_HP = 10000, UP_RANGE = 75, UP_DAMAGE = 100;
 	
 	public Helen(double speed, double atkSpeed, double HP, double range, double damage) {
